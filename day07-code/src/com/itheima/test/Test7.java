7a4c7e7b34c3f04ac0b2cef07376a55800b860b5
squanch <2693115233@qq.com>
2023-12-10 14:01:38 +0800

    1.bestVersion(不使用数组完成数字解密)

--- day07-code/src/com/itheima/test/Test7.java
+++ day07-code/src/com/itheima/test/Test7.java
@@ -1,9 +1,27 @@
 package com.itheima.test;
+
+import java.util.Scanner;
+
 /**
- *
- * */
+ * version1(不使用数组完成数字解密)
+ * 数字解密
+ */
 public class Test7 {
     public static void main(String[] args) {
-        //测试分支
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入加密后的数字");
+        int encryptedNumber = sc.nextInt();
+        int number = 0;
+        while (encryptedNumber != 0) {
+            int digit = encryptedNumber % 10;
+            if (digit >= 0 && digit <= 4) {
+                digit = digit + 10 - 5;
+            } else {
+                digit = digit - 5;
+            }
+            number = number * 10 + digit;
+            encryptedNumber /= 10;
+        }
+        System.out.println("加密前的数字是" + number);
     }
 }
